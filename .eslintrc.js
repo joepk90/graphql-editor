5877403a6c86a0d8556bfa446389d75b69614173
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-04-18 17:44:23 +0100

    first commit - initialise repository

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      alias: {
+        map: [['src', './src']],
+        extensions: ['.ts', '.tsx', '.js'],
+      },
+    },
+  },
+};
