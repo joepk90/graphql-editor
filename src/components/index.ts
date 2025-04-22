f113876e46542e3c0876ae08cf72cc5716f43b02
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-04-22 22:07:32 +0100

    remove files from components barrel export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,9 @@ export * from './TabsContainer';
 export * from './Icons';
 export * from './FakeEditor';
 export * from './Navigation';
-export * from './SchemaEditor';
 export * from './GraphiQLEditor';
 export * from './GraphQLVoyager';
-export * from './Explorer';
+
+// depricated - to do delete
+// export * from './Explorer';
+// export * from './SchemaEditor';
