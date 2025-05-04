18ba358ef610aff986514118f2e1222a99372294
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-04 13:38:57 +0100

    add entrypoint bash script

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# alternative approach theentrypoint.js setup for handling runtime env vars
+
+# Inject runtime config (to set the VITE_API_URL value at run time)
+sed -i "s|__VITE_API_URL__|$VITE_API_URL|g" ./dist/env.runtime.js
+
+# Start the app
+exec serve -s dist -p 8080 --no-clipboard
